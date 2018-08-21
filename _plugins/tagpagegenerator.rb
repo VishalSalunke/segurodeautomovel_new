c12e35e4d8731ae71bff78dae1c79c889484d92f
vishal salunke <salunkevishal05@gmail.com>
2018-08-21 05:59:18 +0000

    Updated '_plugins/tagpagegenerator.rb' via CloudCannon

--- _plugins/tagpagegenerator.rb
+++ _plugins/tagpagegenerator.rb
@@ -1,78 +1,61 @@
 require 'mini_magick'
-
 module Jekyll
-  module JekyllMinimagick
+  class CategoryPage < Page
+    def initialize(site, base, dir, name, pageType)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
 
-    class GeneratedImageFile < Jekyll::StaticFile
-      # Initialize a new GeneratedImage.
-      #   +site+ is the Site
-      #   +base+ is the String path to the <source>
-      #   +dir+ is the String path between <source> and the file
-      #   +name+ is the String filename of the file
-      #   +preset+ is the Preset hash from the config.
-      #
-      # Returns <GeneratedImageFile>
-      def initialize(site, base, dir, name, preset)
-        @site = site
-        @base = base
-        @dir  = dir
-        @name = name
-        @dst_dir = preset.delete('destination')
-        @src_dir = preset.delete('source')
-        @commands = preset
-      end
-
-      # Obtains source file path by substituting the preset's source directory
-      # for the destination directory.
-      #
-      # Returns source file path.
-      def path
-        File.join(@base, @dir.sub(@dst_dir, @src_dir), @name)
-      end
-
-      # Use MiniMagick to create a derivative image at the destination
-      # specified (if the original is modified).
-      #   +dest+ is the String path to the destination dir
-      #
-      # Returns false if the file was not modified since last time (no-op).
-      def write(dest)
-        dest_path = destination(dest)
-
-        return false if File.exist? dest_path and !modified?
-        self.class.mtimes[path] = mtime
-
-        FileUtils.mkdir_p(File.dirname(dest_path))
-        image = ::MiniMagick::Image.open(path)
-        image.combine_options do |c|
-          @commands.each_pair do |command, arg|
-            c.send command, arg
-          end
-        end
-        image.write dest_path
-
-        true
-      end
-
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tagLayout.html')
+      self.data[pageType] = name
+      self.data['title'] = "#{name.to_s.capitalize} - #{Jekyll.configuration({})['webName']}"
+      self.data['postType'] = name
+      self.data['pagination'] = {}
+      self.data['pagination']["enabled"] = true
+      self.data['pagination'][pageType] = name 
+      self.data['description'] = "Artigos relacionados à - #{name.to_s.capitalize}"
+     
     end
+  end
 
-    class MiniMagickGenerator < Generator
-      safe true
+  class TagPageGenerator < Generator
+    safe true
 
-      # Find all image files in the source directories of the presets specified
-      # in the site config.  Add a GeneratedImageFile to the static_files stack
-      # for later processing.
-      def generate(site)
-        return unless site.config['mini_magick']
-
-        site.config['mini_magick'].each_pair do |name, preset|
-          Dir.chdir preset['source'] do
-           Dir.glob(File.join("**", "*.{png,jpg,jpeg,gif}")) do |source|
-              site.static_files << GeneratedImageFile.new(site, site.source, preset['destination'], source, preset.clone)
-             end
+    def generate(site)
+      if site.layouts.key? 'tagLayout'
+          dir = site.config['tag'] || 'tag'
+          skipTags = Jekyll.configuration({})['blacklistedTags'] 
+          site.tags.each_key do |tag|
+            next if skipTags.to_s.downcase.include? tag.to_s.downcase
+            slugTag = tag.to_s.downcase.gsub(" ","-")
+            site.pages << CategoryPage.new(site, site.source, File.join(dir, slugTag), tag,'tag')
           end
+      end
+    end
+  end
+
+
+  class CategoryPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.key? 'tagLayout'
+        site.categories.each_key do |category|
+          case category
+          when "Cidades","Modelos de carro"
+            slugTag = "artigos/"+category.to_s.downcase.gsub(" ","-") 
+          when "Diretório"
+            slugTag = "diretorio"
+          when "Avaliações"
+            slugTag = "diretório"  
+          else
+             slugTag = category.to_s.downcase.gsub(" ","-") 
+          end
+          site.pages << CategoryPage.new(site, site.source, File.join(slugTag), category,'category')
         end
       end
     end
-
   end
 end
